4ce577c3a238f51ebc4046715b9231294a3ee8f0
Eva Maria Vecchi <evamariavecchi@gmail.com>
2023-05-24 16:11:14 +0200

    Update regression_analysis.py

--- scripts/analyze/regression_analysis.py
+++ scripts/analyze/regression_analysis.py
@@ -1,3 +1,13 @@
+def get_ols_analysis(df, dep_variable, ind_variables1, ind_variables2):
+	formula1 = get_formula(dep_variable, ind_variables1)
+	model1 = ols(formula1, data=df).fit()
+	formula2 = get_formula(dep_variable, ind_variables2)
+	model2 = ols(formula2, data=df).fit()
+	print(model1.summary())
+	print(model2.summary())
+	anova_table = sm.stats.anova_lm(model1, model2)
+	print(anova_table)
+
 def get_variable_importance(df, dep_variable, ind_variables):
 	y = df[dep_variable]
 	X = df[ind_variables]
@@ -14,26 +24,6 @@ def get_variable_importance(df, dep_variable, ind_variables):
 		'importance': importances
 		})
 	return(results)
-
-
-def get_ols_analysis(df, dep_variable, ind_variables1, ind_variables2):
-	formula1 = dep_variable + ' ~ ' + ind_variables1[0]
-	x = 1
-	while x in range(len(ind_variables1)):
-		formula1 += ' + ' + ind_variables1[x]
-		x +=1
-	model1 = ols(formula1, data=df).fit()
-	formula2 = dep_variable + ' ~ ' + ind_variables2[0]
-	x = 1
-	while x in range(len(ind_variables2)):
-		formula2 += ' + ' + ind_variables2[x]
-		x +=1
-	model2 = ols(formula2, data=df).fit()
-	print(model1.summary())
-	print(model2.summary())
-	anova_table = sm.stats.anova_lm(model1, model2)
-	print(anova_table)
-
 
 def get_log_regression(df, dep_variable, ind_variables):
 	y = df[dep_variable]
@@ -54,17 +44,20 @@ def get_log_regression(df, dep_variable, ind_variables):
 		})
 	return(m, results)
 
-
 def get_mixed_effects_regression(df, dep_variable, ind_variables, random_variable, group_by):
+	formula = get_formula(dep_variable, ind_variables)
+	formula += ' + (1 | '+random_variable+')'
+	model = smf.mixedlm(formula, data=df, groups = df[group_by])
+	result = model.fit()
+	return(result)
+
+def get_formula(dep_variable, ind_variables):
 	formula = dep_variable + ' ~ ' + ind_variables[0]
 	x = 1
 	while x in range(len(ind_variables)):
 		formula += ' + ' + ind_variables[x]
 		x +=1
-	formula += ' + (1 | '+random_variable+')'
-	model = smf.mixedlm(formula, data=df, groups = df[group_by])
-	result = model.fit()
-	return(result)
+	return(formula)
 
 if __name__ == '__main__':
 	import sys
@@ -96,6 +89,7 @@ if __name__ == '__main__':
 	#	})
 
 	## Example uses for each def:
+	# get_ols_analysis(df, 'gender', ['likes'], ['likes', 'score'])
 	# chi2_info = get_variable_importance(df, 'gender', ['likes','ups','score'])
 	# model, regression_info = get_log_regression(df, 'gender', ['likes','ups','score'])
 	# mixed_effects_info = get_mixed_effects_regression(df, 'gender', ['likes','ups','score'], 'OP_id', 'OP_id')
