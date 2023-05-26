6ca9fa2dc8650960f892c0c9c2ef5b3085c283f1
Eva Maria Vecchi <evamariavecchi@gmail.com>
2023-05-26 16:50:29 +0200

    Update regression_analysis.py

--- scripts/analyze/regression_analysis.py
+++ scripts/analyze/regression_analysis.py
@@ -5,8 +5,17 @@ def get_ols_analysis(df, dep_variable, ind_variables1, ind_variables2):
 	model2 = ols(formula2, data=df).fit()
 	print(model1.summary())
 	print(model2.summary())
-	anova_table = sm.stats.anova_lm(model1, model2)
+	anova=get_anova(model1,model2)
+	print('ANOVA'+anova)
+	return(model1,model2)
+
+def get_ols_analysis_single_m(df, dep_variable, ind_variables):
+	formula = get_formula(dep_variable, ind_variables)
+	model = ols(formula, data=df).fit()
+	print(model.summary())
+	anova_table = sm.stats.anova_lm(model, typ=2)
 	print(anova_table)
+	return(model)
 
 def get_variable_importance(df, dep_variable, ind_variables):
 	y = df[dep_variable]
@@ -31,18 +40,31 @@ def get_log_regression(df, dep_variable, ind_variables):
 	# assess variable importance using F-test
 	# higher scores and lower p-values indicate variables that are more influential in predicting the DV
 	scores, p_values = f_regression(X, y)
-	# logistic regression model to determine variable importance
-	# coef's indicate direction and magnitude of influence of each IV on the target DV
+	Z=df[[dep_variable]+ind_variables]
+	print('Variance Inflation Factor:')
+	print(get_vif(Z))
 	model = LogisticRegression()
 	m = model.fit(X, y)
-	coefficients = m.coef_[0]
 	results = pd.DataFrame({
 		'variable': ind_variables,
-		'score': scores,
-		'p_value': p_values,
-		'coefficient': coefficients
+		'f-reg_score': scores,
+		'f_reg_p_value': p_values
 		})
-	return(m, results)
+	summary = pd.DataFrame({
+		'X': [ind_variables],
+		'y': dep_variable,
+		'intercept': m.intercept_,
+		'coefficients': [m.coef_],
+		'R-squared': m.score(X,y)
+		})
+	return(m, results, summary)
+
+def get_vif(X):
+	vif_data = pd.DataFrame()
+	vif_data["feature"] = X.columns
+	vif_data["VIF"] = [variance_inflation_factor(X.values, i)
+                          for i in range(len(X.columns))]
+	return(vif_data)
 
 def get_mixed_effects_regression(df, dep_variable, ind_variables, random_variable, group_by):
 	formula = get_formula(dep_variable, ind_variables)
@@ -59,11 +81,28 @@ def get_formula(dep_variable, ind_variables):
 		x +=1
 	return(formula)
 
+def get_anova(m1, m2):
+	anova_table = sm.stats.anova_lm(m1,m2)
+	return(anova_table)
+
+def get_model_plot(model, outfile):
+    y_pred = model.fittedvalues
+    residuals = model.resid
+    plt.scatter(y_pred, residuals)
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residuals')
+    plt.title('Residual Plot')
+    plt.axhline(y=0, color='r', linestyle='--')  # Add a horizontal line at y=0
+    plt.savefig(outfile)
+
+
+#TO-DO: add function to get stats of gender column in question, with distribution plot 
+
 if __name__ == '__main__':
-	import sys
+	import sys, json
 	import pandas as pd
+	import numpy as np
 	from pandas import read_csv
-	import researchpy as rp
 	import statsmodels.api as sm
 	import statsmodels.formula.api as smf
 	import scipy.stats as stats
@@ -72,24 +111,36 @@ if __name__ == '__main__':
 	from sklearn.linear_model import LogisticRegression
 	from sklearn.feature_selection import f_regression
 	from statsmodels.formula.api import ols
+	from statsmodels.stats.outliers_influence import variance_inflation_factor
 
-
+	#datafile='/mount/arbeitsdaten14/projekte/sfb-732/d8/falensaa/BiasInArguments/intermediate-data/tal_elal_2016/train_period_data.annotations.jsonlist'
 	datafile = sys.argv[1]
-	df = pd.read_csv(datafile, sep="\t", lineterminator="\n")
-	
-	# NOTE: df['gender'] needs to be [0,1]
-	
-	# Sample data
-	#data = pd.DataFrame({
-	#	'gender': [1, 0, 1, 0, 1, 1, 0, 1, 0],
-	#	'likes': [5, 2, 13, 4, 23, 24, 8, 9, 3],
-	#	'score': [0.87, 0.42, 0.77, 0.23, 0.52, 0.60, 0.40, 0.48, 0.19],
-	#	'ups': [17, 8, 23, 2, 10, 9, 3, 12, 1],
-	#	'OP_id': [12322, 42312, 12322, 72994, 72994, 138284, 298820, 798920, 304091] 
-	#	})
+	with open(datafile, 'r') as f:
+		data = [json.loads(line) for line in f]
 
-	## Example uses for each def:
-	# get_ols_analysis(df, 'gender', ['likes'], ['likes', 'score'])
-	# chi2_info = get_variable_importance(df, 'gender', ['likes','ups','score'])
-	# model, regression_info = get_log_regression(df, 'gender', ['likes','ups','score'])
-	# mixed_effects_info = get_mixed_effects_regression(df, 'gender', ['likes','ups','score'], 'OP_id', 'OP_id')
+	df = pd.DataFrame(data)
+	dep_variable=sys.argv[2]
+	ind_variables=sys.argv[3:]
+
+	print('-----------------------------')
+	print('::Model::')
+	print('Dependent Variable: '+dep_variable)
+	print('Independent Variables: '+str(ind_variables)[1:-1])
+	print('')
+
+	# NOTE: df['gender'] needs to be [0,1]
+	subset = df[df[dep_variable].isin(['M', 'F'])].copy()
+	print('Distribution of Dependent Variable:')
+	print(subset[dep_variable].describe(include='all'))
+	print('')
+	subset[dep_variable] = subset[dep_variable].map({'M': 0, 'F': 1})
+	m,results,summary = get_log_regression(subset, dep_variable, ind_variables)
+	
+	print('')
+	print('Variable Results:')
+	print(results)
+	print('')
+	print('Summary:')
+	print(summary)
+	print('')
+
