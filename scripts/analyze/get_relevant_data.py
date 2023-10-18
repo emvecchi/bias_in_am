9158f61663845464fe873acbf451cdc9d38dfe30
Eva Maria Vecchi <evamariavecchi@gmail.com>
2023-10-18 10:55:21 +0200

    Update get_relevant_data.py

--- scripts/analyze/get_relevant_data.py
+++ scripts/analyze/get_relevant_data.py
@@ -25,11 +25,11 @@ subset = pd.merge(train_subset, feature_df, on='id')
 
 df_acl2022 = get_acl2022_feature_subset(subset)
 
-comment_info = pd.DataFrame(columns=['id', 'perc_explicit_gender_in_comments', 'perc_explicit_gender_in_comments_m', 'perc_explicit_gender_in_comments_f', 'perc_author_gender_in_comments_m', 'perc_author_gender_in_comments_f', 'perc_author_gender_in_comments', 'avg_comment_score'])
+comment_info = pd.DataFrame(columns=['id', 'perc_explicit_gender_in_comments', 'perc_explicit_gender_in_comments_m', 'perc_explicit_gender_in_comments_f', 'perc_author_gender_in_comments_m', 'perc_author_gender_in_comments_f', 'perc_author_gender_in_comments', 'avg_comment_score', 'avg_comment_deltas'])
 for _, row in subset.iterrows():
     comments_df = pd.json_normalize(row['comments'])
     if comments_df.empty:
-        new_entry = {'id':row['id'], 'perc_explicit_gender_in_comments':0.0, 'perc_explicit_gender_in_comments_m':0.0, 'perc_explicit_gender_in_comments_f':0.0, 'perc_author_gender_in_comments_m':0.0, 'perc_author_gender_in_comments_f':0.0, 'perc_author_gender_in_comments':0.0, 'avg_comment_score':0.0}    
+        new_entry = {'id':row['id'], 'perc_explicit_gender_in_comments':0.0, 'perc_explicit_gender_in_comments_m':0.0, 'perc_explicit_gender_in_comments_f':0.0, 'perc_author_gender_in_comments_m':0.0, 'perc_author_gender_in_comments_f':0.0, 'perc_author_gender_in_comments':0.0, 'avg_comment_score':0.0, 'avg_comment_deltas':0.0}    
     else:
         comment_count = float(comments_df.shape[0])
         explicit_gender_m = (comments_df['explicit_gender'] == 'M').sum() / comment_count
@@ -39,7 +39,10 @@ for _, row in subset.iterrows():
         author_gender_f = (comments_df['author_gender'] == 'F').sum() / comment_count
         perc_author_mentions = comments_df['author_gender'].isin(['M', 'F']).sum() / comment_count
         avg_comment_score = comments_df['score'].sum() / comment_count
-        new_entry = {'id':row['id'], 'perc_explicit_gender_in_comments':perc_explicit_mentions, 'perc_explicit_gender_in_comments_m':explicit_gender_m, 'perc_explicit_gender_in_comments_f':explicit_gender_f, 'perc_author_gender_in_comments_m':author_gender_m, 'perc_author_gender_in_comments_f':author_gender_f, 'perc_author_gender_in_comments':perc_author_mentions, 'avg_comment_score':avg_comment_score}
+        comments_df['author_flair_text'] = comments_df['author_flair_text'].astype(str)
+        comments_df['author_flair_text'] = pd.to_numeric(comments_df['author_flair_text'].str[:-1], errors='coerce')
+        avg_comment_deltas = comments_df['author_flair_text'].sum() / comment_count
+        new_entry = {'id':row['id'], 'perc_explicit_gender_in_comments':perc_explicit_mentions, 'perc_explicit_gender_in_comments_m':explicit_gender_m, 'perc_explicit_gender_in_comments_f':explicit_gender_f, 'perc_author_gender_in_comments_m':author_gender_m, 'perc_author_gender_in_comments_f':author_gender_f, 'perc_author_gender_in_comments':perc_author_mentions, 'avg_comment_score':avg_comment_score, 'avg_comment_deltas': avg_comment_deltas}
     comment_info = pd.concat([comment_info, pd.DataFrame([new_entry])], ignore_index=True)
 
 
@@ -68,7 +71,10 @@ full_tan2016_features = [
 
 df_acl2022_tan2016 = pd.merge(df_acl2022, feature_df[['id']+full_tan2016_features], on='id', how='left')
 
-df = pd.merge(df_acl2022_tan2016, subset[['id','author_gender', 'explicit_gender', 'num_comments', 'score', 'edited_binary', 'topic']], on='id', how='left')
+subset['author_flair_text'] = subset['author_flair_text'].astype(str)
+subset['deltas'] = pd.to_numeric(subset['author_flair_text'].str[:-1], errors='coerce')
+
+df = pd.merge(df_acl2022_tan2016, subset[['id','author_gender', 'explicit_gender', 'num_comments', 'score', 'edited_binary', 'topic', 'deltas']], on='id', how='left')
 
 df = pd.merge(df,comment_info, on='id', how='left')
 
